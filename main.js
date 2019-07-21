f0d640aaa48eefb13e605e941a26d6f5b6dd8556
FluffyF1sh <37347360+FluffyF1sh@users.noreply.github.com>
2019-07-21 20:43:25 +0400

    upd33

--- main.js
+++ main.js
@@ -2,27 +2,54 @@ var data = null;
 var xhr = new XMLHttpRequest();
 var all = document.createElement('div');
 document.body.appendChild(all);
-var s = document.createElement('div');
-var s1 = document.createElement('div');
-var s2 = document.createElement('div');
+var title = document.createElement('div');
+var sum = document.createElement('div');
+var users = document.createElement('div');
+var opt1 = document.createElement('div');
+var opt2 = document.createElement('div');
 var arr;
-all.classList.add('all');
 xhr.addEventListener("readystatechange", function() {
   if (this.readyState === this.DONE) {
-    console.log(this.response);
-    s.textContent =  this.response;
-	s1.textContent = s.textContent;
-	s2.textContent = s.textContent;
+    //console.log(this.response);
+    title.textContent =  this.response;
+	sum.textContent = this.response;
+	users.textContent = this.response;
+	opt1.textContent = this.response;
+	opt2.textContent = this.response;
 	arr = this.response.split(',');
-	s.textContent = (arr[4].replace('"title":',""));
-	s1.textContent = (arr[1].replace('"totalAmount"',"Общая сумма"));
-	s2.textContent = (arr[2].replace('"totalUsers"',"Ставочников"));
+	var act = '{"active":null';
+	var check1 = '"winner":true}';
+	var check2 = '"winner":true}]';
+	var winRes;
+	console.log(arr[14],arr[19]);
+	if(act == arr[0]){
+		title.textContent = ' ';
+		if(check1 == arr[14]){
+			winRes = arr[12];
+		}
+		else if(check2 == arr[19]){
+			winRes = arr[17];
+		}
+		sum.textContent = 'Соревнование не проводится. ' +'Прошлый победитель: ' + winRes.replace('"title":', '').replace('"','').replace('"','');
+		users.textContent = ' ';
+		opt1.textContent = ' ';
+		opt2.textContent = ' ';
+	}
+	else{
+		sum.textContent = (arr[1].replace('"totalAmount"',"Общая сумма"));
+		title.textContent = (arr[4].replace('"title":',"").replace('"', '').replace('"', ""));
+		users.textContent = (arr[2].replace('"totalUsers"',"Ставочников"));
+		opt1.textContent = ("!bet " + arr[12].replace('"command":', "").replace('"', '').replace('"}', "") + " [сумма]");
+		opt2.textContent = ("!bet " + arr[17].replace('"command":', "").replace('"', '').replace('"}]', "") + " [сумма]");;
+	}
+	console.log(check1);
   }
 });
-setTimeout("window.location.reload()", 5000);
-all.appendChild(s); 
-all.appendChild(s1);
-all.appendChild(s2);
+all.appendChild(title); 
+all.appendChild(sum);
+all.appendChild(users);
+all.appendChild(opt1);
+all.appendChild(opt2);
 xhr.open("GET", "https://api.streamelements.com/kappa/v2/contests/5d26328e6b84c45a4fe1845a");
 xhr.setRequestHeader("accept", "application/json");
 
